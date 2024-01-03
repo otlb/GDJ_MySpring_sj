c5c0658478e4c1e7e51e905685ca2b70a08998d9
parksungjea <sungjea15@naver.com>
2024-01-03 15:18:46 +0900

    123123

---
+++ src/main/java/com/winter/app/product/ProductDAO.java
@@ -0,0 +1,38 @@
+package com.winter.app.product;
+
+import java.util.List;
+
+public class ProductDAO {
+
+	
+	
+	
+	public ProductDTO  detail(ProductDTO bankDTO) {
+			
+		return null;
+	}
+	
+	
+	public List<ProductDTO> list() {	 
+		
+		return null;
+	}
+	public int add(ProductDTO bnBankDTO) {
+		
+		return 0;
+		
+	}
+	
+	public int delete(ProductDTO bankDTO) {
+		
+		return 0;
+		
+	}
+	
+	public int update(ProductDTO bankDTO) {
+		
+		return 0;
+		
+	}
+	
+}
---
+++ src/main/java/com/winter/app/product/productController.java
@@ -0,0 +1,24 @@
+package com.winter.app.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/product/*")
+public class productController {
+
+	@RequestMapping(value="list", method = RequestMethod.GET)
+	public void list() {
+	
+		
+	}
+	
+	
+	@RequestMapping(value="detail", method = RequestMethod.GET)
+	public void detail(ProductDTO productDTO) {
+		
+		
+	}
+	
+}
