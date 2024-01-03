16263535c1d3bb3a9490aa1899095bc110157951
parksungjea <sungjea15@naver.com>
2024-01-03 19:46:36 +0900

    123123

--- src/main/java/com/winter/app/product/productController.java
+++ src/main/java/com/winter/app/product/productController.java
@@ -5,20 +5,23 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value="/product/*")
 public class productController {
-
+	private ProductDAO productDAO ;
+	
+	public productController() {
+		this.productDAO = new ProductDAO();
+	}
+	
 	@RequestMapping(value="list", method = RequestMethod.GET)
-
-
-
 	public String list(HttpServletRequest request)	throws Exception {
-		ProductDAO productDAO = new ProductDAO();
 		List<ProductDTO> ar = productDAO.list();
 		request.setAttribute("list", ar);
 		
@@ -31,11 +34,12 @@ public class productController {
 	
 	
 	@RequestMapping(value="detail", method = RequestMethod.GET)
-	public void detail(ProductDTO productDTO) throws Exception{
-		ProductDAO productDAO = new ProductDAO();
+	public String detail(ProductDTO productDTO, Model model) throws Exception{
+		productDTO = productDAO.detail(productDTO);
+	
+		model.addAttribute("dto", productDTO);
 		
-		productDAO.detail(productDTO);
-		
+		return "product/detail";
 	}
 	
 }
